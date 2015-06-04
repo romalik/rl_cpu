a51860c1e4b9c550e645c3c9dca20eda5118a8cd
Roman Likane <r.likane@robocv.ru>
2015-06-04 20:42:46 +0300

    +oplist

---
+++ v3/oplist.h
@@ -0,0 +1,93 @@
+char oplist[][16] = {
+    "nop",
+
+    "addrf_b",
+    "addrf_w",
+    "iaddrf_b",
+    "iaddrf_w",
+
+    "addrl_b",
+    "addrl_w",
+    "iaddrl_b",
+    "iaddrl_w",
+
+    "cnst_b",
+    "cnst_w",
+    "icnst_b",
+    "icnst_w",
+
+    "addrs_b",
+    "addrs_w",
+    "iaddrs_b",
+    "iaddrs_w",
+
+    "indir",
+
+    "neg",
+
+    "add",
+    "add_b",
+    "add_w",
+
+    "band",
+    "band_b",
+    "band_w",
+
+    "bor",
+    "bor_b",
+    "bor_w",
+
+    "bxor",
+    "bxor_b",
+    "bxor_w",
+
+    "lsh",
+    "lsh_b",
+    "lsh_w",
+
+    "rsh",
+    "rsh_b",
+    "rsh_w",
+
+    "sub",
+    "sub_b",
+    "sub_w",
+
+    "eq_w",
+    "ge_w",
+    "gt_w",
+    "le_w",
+    "lt_w",
+    "ne_w",
+
+    "ueq_w",
+    "uge_w",
+    "ugt_w",
+    "ule_w",
+    "ult_w",
+    "une_w",
+
+    "call_w",
+    "call",
+
+    "ret",
+
+    "jump_w",
+    "jump",
+
+    "discard1",
+    "discard_b",
+    "discard_w",
+
+    "alloc1",
+    "alloc_b",
+    "alloc_w",
+
+    "fastcall",
+    "fastcall_w",
+
+    "store",
+
+    ""
+
+};
